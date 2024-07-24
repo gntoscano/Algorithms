29879b06565377215e9f0b317699c9e4b9f6b931
Gabriel Toscano <toscanogabriel55@gmail.com>
2024-07-24 20:16:56 -0300

    Create hash-firsttry.py
    First try to make a hash table

---
+++ Hash_Tables/hash-firsttry.py
@@ -0,0 +1,13 @@
+#First attempt to make a hash table -- READ A STRING AND RETURN A NUMBER
+
+lista_telefonica = dict()
+
+lista_telefonica["Gabriel"] = 82255687
+lista_telefonica["Fernando"] = 76542096
+lista_telefonica["Emergency"] = 911
+
+#Fast way to get data. Take care with the colisions
+
+print(lista_telefonica["Gabriel"])
+print(lista_telefonica["Fernando"])
+print(lista_telefonica["Emergency"])
