5fc47cf585b456519a13c05c8e4340c4fc279fce
Gabriel Toscano <toscanogabriel55@gmail.com>
2024-10-04 22:53:35 -0300

    Created the insertion node function

--- Lists/simplelist.c
+++ Lists/simplelist.c
@@ -12,5 +12,13 @@ Node* creationNode(int data)
     Node *newNode = (Node*)malloc(sizeof(Node));
     newNode -> data = data;
     newNode -> next = NULL; //The last item of a list have to point NULL
-
 }
+
+//Insert a new node at the beginning of the list
+void insertNode(Node **head, int data)
+{
+    Node newNode = creationNode(data);
+    newNode -> next = *head;
+    *head = newNode;
+}
+
